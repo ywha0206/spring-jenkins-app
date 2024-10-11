863ea48d725bacc1cd9731518c4e2dc3c4def0e4
ywha0206 <ppyyhh3285@gmail.com>
2024-10-11 12:08:17 +0900

    first commit

---
+++ src/main/java/co/kr/jenkins/springjenkinsapp/SpringJenkinsAppApplication.java
@@ -0,0 +1,13 @@
+package co.kr.jenkins.springjenkinsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJenkinsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/kr/jenkins/springjenkinsapp/SpringJenkinsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.jenkins.springjenkinsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
